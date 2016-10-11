4425f1664b200a6004cea72be17472305c800d2f
NoahBragg <normdoow@yahoo.com>
2016-10-10 20:58:44 -0400

    added Audio Player and playing through Fiery Furnace images and audio

--- app/src/main/java/org/sil/storyproducer/controller/learn/LearnActivity.java
+++ app/src/main/java/org/sil/storyproducer/controller/learn/LearnActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 import org.sil.storyproducer.R;
+import org.sil.storyproducer.tools.AudioPlayer;
 import org.sil.storyproducer.tools.FileSystem;
 
 import java.io.IOException;
@@ -13,28 +14,42 @@ import java.io.IOException;
 public class LearnActivity extends AppCompatActivity {
 
     ImageView learnImageView;
+    int slideNum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn);
 
+        //turn on the background music
+        AudioPlayer backgroundPlayer = new AudioPlayer();
+        backgroundPlayer.playWithPath(FileSystem.getStoryPath("Fiery Furnace") + "/SoundTrack0.mp3");
+        backgroundPlayer.setVolume(0.4f);
+
         //get the image view and change the image
         learnImageView = (ImageView) findViewById(R.id.learnImageView);
-        learnImageView.setImageBitmap(FileSystem.getImage("Fiery Furnace", 6));
 
-        //play the audio for this scene
-        MediaPlayer mPlayer = new MediaPlayer();
-        try {
-            mPlayer.setDataSource(FileSystem.getStoryPath("Fiery Furnace") + "/narration6.wav");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            mPlayer.prepare();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        mPlayer.start();
+        playVideo();
     }
+
+    /**
+     * Plays the video and runs everytime the audio is completed
+     */
+    void playVideo() {
+        System.out.println("The audio stopped");
+        learnImageView.setImageBitmap(FileSystem.getImage("Fiery Furnace", slideNum));          //set the next image
+        AudioPlayer aPlayer = new AudioPlayer();                                                //set the next audio
+        aPlayer.playWithPath(FileSystem.getStoryPath("Fiery Furnace") + "/narration" + slideNum + ".wav");
+        aPlayer.audioCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                //TODO: change this to actually count the amount of files
+                if(slideNum < 23) {
+                    playVideo();
+                }
+            }
+        });
+        slideNum++;         //move to the next slide
+    }
+
 }
---
+++ app/src/main/java/org/sil/storyproducer/tools/AudioPlayer.java
@@ -0,0 +1,89 @@
+package org.sil.storyproducer.tools;
+
+import android.media.MediaPlayer;
+import android.net.sip.SipAudioCall;
+
+import java.io.IOException;
+
+/**
+ * Created by noahbragg on 10/10/16.
+ */
+
+public class AudioPlayer {
+
+    MediaPlayer mPlayer;
+
+    /**
+     * Constructor for Audio Player. Takes no params
+     */
+    public AudioPlayer() {
+        mPlayer = new MediaPlayer();
+    }
+
+    /**
+     * Plays the audio with the given path
+     * @param  :String for the path where the audio resides
+     */
+    public void playWithPath(String path) {
+        try {
+            mPlayer.setDataSource(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            mPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mPlayer.start();
+    }
+
+    /**
+     * Pauses the audio if it is currenlty being played
+     */
+    public void pauseAudio() {
+        if(mPlayer.isPlaying()) {
+            try {
+                mPlayer.pause();
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Stops the audio if it is currenlty being played
+     */
+    public void stopAudio() {
+        try {
+            mPlayer.stop();
+            mPlayer.release();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * returns the duration of the audio as an int
+     * @return returns the duration of the audio as an int
+     */
+    public int getAudioDurationInSeconds() {
+        return (int)(mPlayer.getDuration() * 0.001);
+    }
+
+    /**
+     * sets the completion listener
+     * @param listener
+     */
+    public void audioCompletionListener(MediaPlayer.OnCompletionListener listener) {
+        mPlayer.setOnCompletionListener(listener);
+    }
+
+    /**
+     * sets the volume of the audio
+     * @param :the float for the volume form 0.0 to 1.0
+     */
+    public void setVolume(float volume) {
+        mPlayer.setVolume(volume, volume);
+    }
+}
