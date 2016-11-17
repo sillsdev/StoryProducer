36a5ee2e8c2c6363a4f2cbac2504321e20b9ca57
Grant Dennison <grant.dennison@gmail.com>
2016-11-17 01:47:13 -0500

    Constructed untested pipeline for VideoStoryMaker

--- app/src/main/java/org/sil/storyproducer/media/VideoStoryMaker.java
+++ app/src/main/java/org/sil/storyproducer/media/VideoStoryMaker.java
@@ -4,7 +4,16 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.media.MediaFormat;
+import android.media.MediaMuxer;
 
+import org.sil.storyproducer.media.pipe.PipedAudioConcatenator;
+import org.sil.storyproducer.media.pipe.PipedAudioMixer;
+import org.sil.storyproducer.media.pipe.PipedAudioResampler;
+import org.sil.storyproducer.media.pipe.PipedMediaDecoder;
+import org.sil.storyproducer.media.pipe.PipedMediaEncoder;
+import org.sil.storyproducer.media.pipe.PipedMediaExtractor;
+import org.sil.storyproducer.media.pipe.PipedMediaMuxer;
+import org.sil.storyproducer.media.pipe.PipedVideoSurfaceEncoder;
 import org.sil.storyproducer.media.pipe.PipedVideoSurfaceSource;
 import org.sil.storyproducer.media.pipe.SourceUnacceptableException;
 
@@ -12,6 +21,8 @@ import java.io.File;
 import java.io.IOException;
 
 public class VideoStoryMaker implements PipedVideoSurfaceSource {
+    private File mOutputFile;
+
     private MediaFormat mVideoFormat;
     private MediaFormat mAudioFormat;
     private StoryPage[] mPages;
@@ -21,6 +32,8 @@ public class VideoStoryMaker implements PipedVideoSurfaceSource {
     private int mCurrentPageIndex = -1;
     private long mCurrentPageDuration = 0;
     private long mCurrentPageStart = 0;
+
+    private int mSampleRate;
 
     private int mFrameRate;
 
@@ -32,18 +45,62 @@ public class VideoStoryMaker implements PipedVideoSurfaceSource {
 
     private boolean mIsVideoDone = false;
 
-    public VideoStoryMaker(MediaFormat videoFormat, MediaFormat audioFormat, StoryPage[] pages, File soundtrack, long delayUs) {
+    public VideoStoryMaker(File output, MediaFormat videoFormat, MediaFormat audioFormat, StoryPage[] pages, File soundtrack, long delayUs) {
+        mOutputFile = output;
         mVideoFormat = videoFormat;
         mAudioFormat = audioFormat;
         mPages = pages;
         mSoundTrack = soundtrack;
         mDelayUs = delayUs;
 
+        mSampleRate = mAudioFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE);
+
         mFrameRate = mVideoFormat.getInteger(MediaFormat.KEY_FRAME_RATE);
 
         mWidth = mVideoFormat.getInteger(MediaFormat.KEY_WIDTH);
         mHeight = mVideoFormat.getInteger(MediaFormat.KEY_HEIGHT);
         mScreenRect = new Rect(0, 0, mWidth, mHeight);
+    }
+
+    public void churn() {
+        try {
+            PipedMediaMuxer muxer = new PipedMediaMuxer(mOutputFile.getPath(), MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+
+            PipedMediaEncoder audioEncoder = new PipedMediaEncoder(mAudioFormat);
+            muxer.addSource(audioEncoder);
+
+            PipedAudioMixer audioMixer = new PipedAudioMixer();
+            audioEncoder.addSource(audioMixer);
+
+            PipedAudioResampler soundtrackResampler = new PipedAudioResampler(mSampleRate, 1);
+            audioMixer.addSource(soundtrackResampler);
+
+            PipedMediaDecoder soundtrackDecoder = new PipedMediaDecoder();
+            soundtrackResampler.addSource(soundtrackDecoder);
+
+            PipedMediaExtractor soundtrackExtractor = new PipedMediaExtractor(mSoundTrack.getPath(), MediaHelper.MediaType.AUDIO);
+            soundtrackDecoder.addSource(soundtrackExtractor);
+
+            PipedAudioConcatenator narrationConcatenator = new PipedAudioConcatenator(1000);
+            for (StoryPage page : mPages) {
+                PipedAudioResampler narrationResampler = new PipedAudioResampler(mSampleRate, 1);
+                narrationConcatenator.addSource(narrationResampler);
+
+                PipedMediaDecoder narrationDecoder = new PipedMediaDecoder();
+                narrationResampler.addSource(narrationDecoder);
+
+                PipedMediaExtractor narrationExtractor = new PipedMediaExtractor(page.getNarrationAudio().getPath(), MediaHelper.MediaType.AUDIO);
+                narrationDecoder.addSource(narrationExtractor);
+            }
+
+            PipedVideoSurfaceEncoder videoEncoder = new PipedVideoSurfaceEncoder();
+            muxer.addSource(videoEncoder);
+
+            videoEncoder.addSource(this);
+        }
+        catch (IOException | SourceUnacceptableException | RuntimeException e) {
+            e.printStackTrace();
+        }
     }
 
     private void drawFrame(Canvas canv, int pageIndex, long timeOffset, float alpha) {
