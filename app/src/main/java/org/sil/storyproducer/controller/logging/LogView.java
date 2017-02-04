ce54de9e358e4843d80375230a2ac6531d0aee64
MichaelBaxter9 <michaelbaxter9@gmail.com>
2017-02-04 17:03:03 -0500

    Made some progress on the logging system. Started making an activity to display logs. It is currently very much a work-in-progress.

--- app/src/main/java/org/sil/storyproducer/controller/logging/LogView.java
+++ app/src/main/java/org/sil/storyproducer/controller/logging/LogView.java
@@ -2,13 +2,56 @@ package org.sil.storyproducer.controller.logging;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
 import org.sil.storyproducer.R;
 
 public class LogView extends AppCompatActivity {
+
+
+    private ListView listView = null;
+
+    private class LogListAdapter extends BaseAdapter {
+
+        private LogEntry[] logEntries;
+
+        public LogListAdapter(Log log){
+            this.logEntries=log.toArray(logEntries);
+        }
+
+        @Override
+        public int getCount() {
+            return logEntries.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return logEntries[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_view);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        setTitle("Logging!!!");
+        listView = (ListView) findViewById(R.id.log_list_view);
+        listView.setAdapter(null);
     }
 }
