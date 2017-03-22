be61a161af014104f5fe648f2dd5f13d00119073
Grant Dennison <grant.dennison@gmail.com>
2017-03-21 22:41:46 -0400

    Fixed audio quality issue by increasing bit rate to reasonable value.

--- app/src/main/java/org/sil/storyproducer/tools/media/AudioRecorder.java
+++ app/src/main/java/org/sil/storyproducer/tools/media/AudioRecorder.java
@@ -11,6 +11,13 @@ import android.support.v4.content.ContextCompat;
  * Thin wrapper for {@link MediaRecorder} which provides some default behavior for recorder.
  */
 public class AudioRecorder extends MediaRecorder {
+    //See https://developer.android.com/guide/topics/media/media-formats.html for supported formats.
+    private static final int OUTPUT_FORMAT = OutputFormat.MPEG_4;
+    private static final int AUDIO_ENCODER = AudioEncoder.AAC;
+    private static final int SAMPLE_RATE = 44100;
+    private static final int BIT_DEPTH = 16;
+    //Set bit rate to exact spec of Android doc or to SAMPLE_RATE * BIT_DEPTH.
+    private static final int BIT_RATE = SAMPLE_RATE * BIT_DEPTH;
 
     public AudioRecorder(String filename, Activity activity) {
         super();
@@ -23,10 +30,10 @@ public class AudioRecorder extends MediaRecorder {
         }
 
         setAudioSource(MediaRecorder.AudioSource.MIC);
-        setOutputFormat(OutputFormat.MPEG_4);
-        setAudioEncoder(AudioEncoder.AAC);
-        setAudioEncodingBitRate(16);
-        setAudioSamplingRate(44100);
+        setOutputFormat(OUTPUT_FORMAT);
+        setAudioEncoder(AUDIO_ENCODER);
+        setAudioEncodingBitRate(BIT_RATE);
+        setAudioSamplingRate(SAMPLE_RATE);
         setOutputFile(filename);
     }
 }
