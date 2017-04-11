7977ece5418d6e468ed8259136892b41013cfc03
andrewlockridge <lockdrew95@gmail.com>
2017-04-11 16:23:26 -0400

    Consultant password changed to "appr00ved"

--- app/src/main/java/org/sil/storyproducer/controller/consultant/ConsultantCheckFrag.java
+++ app/src/main/java/org/sil/storyproducer/controller/consultant/ConsultantCheckFrag.java
@@ -48,7 +48,7 @@ public class ConsultantCheckFrag extends Fragment {
     public static final String CONSULTANT_PREFS = "Consultant_Checks";
     public static final String IS_CONSULTANT_APPROVED = "isApproved";
     private static final String IS_CHECKED = "isChecked";
-    private static final String PASSWORD = "password";
+    private static final String PASSWORD = "appr00ved";
     private String storyName;
     private int slidePosition;
     private View rootView;
