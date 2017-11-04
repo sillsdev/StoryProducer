9508972ce9d79399282ac10a78e39c58706c68f3
Ann C <annie.pikapal@gmail.com>
2017-11-04 14:45:33 -0400

    added a basic layout for Submission Remote Consultant Phase
    basica layout and empty class added for the Submission Remote Consultant phase (mainly for design document purposes)

---
+++ app/src/main/java/org/sil/storyproducer/controller/remote/SubmissionRemoteConsultant.java
@@ -0,0 +1,11 @@
+package org.sil.storyproducer.controller.remote;
+
+import org.sil.storyproducer.controller.phase.PhaseBaseActivity;
+
+/**
+ * Created by annmcostantino on 11/4/2017.
+ */
+
+public class SubmissionRemoteConsultant extends PhaseBaseActivity {
+
+}
