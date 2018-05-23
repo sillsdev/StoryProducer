0e0bc3f22dbe44faff8122fce6b86d1e6140fbe4
John Lambert <johnml1135@gmail.com>
2018-05-23 16:19:31 -0400

    Initial build with Kotlin files for the Template and Template Slide thingys.  It compiles and opens up.

--- app/src/main/java/org/sil/storyproducer/tools/file/KenBurnsSpec.java
+++ app/src/main/java/org/sil/storyproducer/tools/file/KenBurnsSpec.java
@@ -2,7 +2,7 @@ package org.sil.storyproducer.tools.file;
 
 import android.graphics.Rect;
 
-import org.sil.storyproducer.model.Template;
+import org.sil.storyproducer.model.TemplateKt;
 import org.sil.storyproducer.model.TemplateSlide;
 import org.sil.storyproducer.tools.media.graphics.BitmapHelper;
 import org.sil.storyproducer.tools.media.graphics.KenBurnsEffect;
@@ -12,7 +12,7 @@ import java.io.File;
 
 public class KenBurnsSpec {
     public static KenBurnsEffect getKenBurnsEffect(String story, int index) {
-        TemplateSlide slide = Template.getSlide(story, index);
+        TemplateSlide slide = TemplateKt.TemplateSlides(story, index);
         if(slide != null) {
             return slide.getKenBurnsEffect();
         }
