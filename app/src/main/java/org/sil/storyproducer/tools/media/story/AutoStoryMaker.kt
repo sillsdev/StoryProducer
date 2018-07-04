83052fb3b51c0eef49a14a1ea422b49adca3fdc1
John Lambert <johnml1135@gmail.com>
2018-07-04 09:46:49 -0400

    This needs to be updated as well.  Converted to Kotlin.

---
+++ app/src/main/java/org/sil/storyproducer/tools/media/story/AutoStoryMaker.kt
@@ -0,0 +1,360 @@
+package org.sil.storyproducer.tools.media.story
+
+import android.content.Context
+import android.media.MediaCodecInfo
+import android.media.MediaFormat
+import android.media.MediaMuxer
+import android.util.Log
+import android.widget.Toast
+
+import org.sil.storyproducer.model.Workspace
+import org.sil.storyproducer.tools.file.AudioFiles
+import org.sil.storyproducer.tools.file.FileSystem
+import org.sil.storyproducer.tools.file.ImageFiles
+import org.sil.storyproducer.tools.file.KenBurnsSpec
+import org.sil.storyproducer.tools.file.TextFiles
+import org.sil.storyproducer.tools.file.VideoFiles
+import org.sil.storyproducer.tools.media.MediaHelper
+import org.sil.storyproducer.tools.media.graphics.KenBurnsEffect
+import org.sil.storyproducer.tools.media.graphics.TextOverlay
+import org.sil.storyproducer.tools.media.graphics.TextOverlayHelper
+
+import java.io.Closeable
+import java.io.File
+import java.io.IOException
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+/**
+ * AutoStoryMaker is a layer of abstraction above [StoryMaker] that handles all of the
+ * parameters for StoryMaker according to some defaults, structure of projects/templates, and
+ * minimal customization.
+ */
+class AutoStoryMaker : Thread(), Closeable {
+
+    private val mStory: String
+    private var mTitle: String? = null
+
+    // size of a frame, in pixels
+    // first size isn't great quality, but runs faster
+    private var mWidth = 320
+    private var mHeight = 240
+
+    private var mOutputExt = ".mp4"
+    private var mOutputFile: File? = null
+    private var mTempFile: File? = null
+    // bits per second for video
+    private var mVideoBitRate: Int = 0
+
+    private var mIncludeBackgroundMusic = true
+    private var mIncludePictures = true
+    private var mIncludeText = false
+    private var mIncludeKBFX = true
+
+    private var mLogProgress = false
+
+    private var mStoryMaker: StoryMaker? = null
+
+    private var mContext: Context? = null
+
+    val isDone: Boolean
+        get() = mStoryMaker != null && mStoryMaker!!.isDone
+
+    val progress: Double
+        get() = if (mStoryMaker == null) {
+            0.0
+        } else {
+            mStoryMaker!!.progress
+        }
+
+    init {
+        mStory = Workspace.activeStory.title
+        mTitle = mStory
+
+        setResolution(mWidth, mHeight)
+
+        val outputDir = VideoFiles.getDefaultLocation(mStory)
+        setOutputFile(File(outputDir, mStory.replace(' ', '_') + "_"
+                + mWidth + "x" + mHeight + mOutputExt))
+    }
+
+    /**
+     * Set a context for the AutoStoryMaker if [android.widget.Toast] error messages are desired.
+     * If no context is set, console logging will be used instead.
+     *
+     * @param context
+     */
+    fun setContext(context: Context) {
+        mContext = context
+    }
+
+    fun setOutputFile(output: File) {
+        mOutputFile = output
+
+        val extPattern = Pattern.compile("\\.\\w+$")
+        val extMatcher = extPattern.matcher(mOutputFile!!.name)
+        extMatcher.find()
+        mOutputExt = extMatcher.group()
+
+        mTempFile = File(VideoFiles.getTempLocation(mStory), "partial_video$mOutputExt")
+    }
+
+    fun setResolution(width: Int, height: Int) {
+        mWidth = width
+        mHeight = height
+
+        val pixelRate = mWidth * mHeight * VIDEO_FRAME_RATE
+        mVideoBitRate = (pixelRate.toFloat() * MOTION_FACTOR.toFloat() * KUSH_GAUGE_CONSTANT).toInt()
+    }
+
+    fun setTitle(title: String) {
+        mTitle = title
+    }
+
+    fun toggleBackgroundMusic(includeBackgroundMusic: Boolean) {
+        mIncludeBackgroundMusic = includeBackgroundMusic
+    }
+
+    fun togglePictures(includePictures: Boolean) {
+        mIncludePictures = includePictures
+    }
+
+    fun toggleText(includeText: Boolean) {
+        mIncludeText = includeText
+    }
+
+    fun toggleKenBurns(includeKBFX: Boolean) {
+        mIncludeKBFX = includeKBFX
+    }
+
+    /**
+     * Set whether progress is periodically logged in console. Default is false (no console logging).
+     * @param logProgress whether progress should be logged in console.
+     */
+    fun toggleLogProgress(logProgress: Boolean) {
+        mLogProgress = logProgress
+    }
+
+    override fun start() {
+        val outputFormat = MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4
+
+        val audioFormat = generateAudioFormat()
+        val videoFormat = generateVideoFormat()
+        val pages = generatePages() ?: return
+
+        //If pages weren't generated, exit.
+
+        mStoryMaker = StoryMaker(mTempFile, outputFormat, videoFormat, audioFormat,
+                pages, AUDIO_TRANSITION_US, SLIDE_CROSS_FADE_US)
+
+        watchProgress()
+
+        super.start()
+    }
+
+    override fun run() {
+        var duration = -System.currentTimeMillis()
+
+        Log.i(TAG, "Starting video creation")
+        val success = mStoryMaker!!.churn()
+
+        if (success) {
+            Log.v(TAG, "Moving completed video to " + mOutputFile!!.absolutePath)
+            mTempFile!!.renameTo(mOutputFile)
+        } else {
+            Log.w(TAG, "Deleting incomplete temporary video")
+            mTempFile!!.delete()
+        }
+
+        duration += System.currentTimeMillis()
+
+        Log.i(TAG, "Stopped making story after "
+                + MediaHelper.getDecimal(duration / 1000.toDouble()) + " seconds")
+    }
+
+    private fun generateAudioFormat(): MediaFormat {
+        val audioFormat = MediaHelper.createFormat(AUDIO_MIME_TYPE)
+        audioFormat.setInteger(MediaFormat.KEY_AAC_PROFILE, MediaCodecInfo.CodecProfileLevel.AACObjectLC)
+        audioFormat.setInteger(MediaFormat.KEY_BIT_RATE, AUDIO_BIT_RATE)
+        audioFormat.setInteger(MediaFormat.KEY_SAMPLE_RATE, AUDIO_SAMPLE_RATE)
+        audioFormat.setInteger(MediaFormat.KEY_CHANNEL_COUNT, AUDIO_CHANNEL_COUNT)
+
+        return audioFormat
+    }
+
+    private fun generateVideoFormat(): MediaFormat? {
+        //If no video component, use null format.
+        if (!mIncludePictures && !mIncludeText) {
+            return null
+        }
+
+        val videoFormat = MediaFormat.createVideoFormat(VIDEO_MIME_TYPE, mWidth, mHeight)
+        videoFormat.setInteger(MediaFormat.KEY_FRAME_RATE, VIDEO_FRAME_RATE)
+        videoFormat.setInteger(MediaFormat.KEY_BIT_RATE, mVideoBitRate)
+        videoFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, VIDEO_IFRAME_INTERVAL)
+
+        return videoFormat
+    }
+
+    private fun generatePages(): Array<StoryPage>? {
+        val slideCount = FileSystem.getContentSlideAmount(mStory)
+        //TODO: add hymn to count
+        val pages: MutableList<StoryPage> = mutableListOf()
+
+        val widthToHeight = mWidth / mHeight.toDouble()
+
+        //Create title slide with overlayed text.
+        //Try to get special background (user provided).
+        var titleBack = ImageFiles.getFile(mStory, ImageFiles.TITLE_BACKGROUND)
+        if (!titleBack.exists()) {
+            //Fallback to standard title slide.
+            titleBack = ImageFiles.getFile(mStory, 0)
+        }
+
+        //Default title with no text.
+        var title = titleBack
+
+        val tempTitle = ImageFiles.getFile(mStory, ImageFiles.TITLE_TEMP)
+        val titleOverlay = TextOverlay(mTitle)
+        titleOverlay.setFontSize(TITLE_FONT_SIZE)
+
+        try {
+            TextOverlayHelper.overlayJPEG(titleBack, tempTitle, titleOverlay)
+            title = tempTitle
+        } catch (e: IOException) {
+            Log.w(TAG, "Failed to create overlayed title slide!")
+        }
+
+        var lastSoundtrack: File? = null
+        var iSlide: Int
+        iSlide = 0
+        while (iSlide < slideCount) {
+            var image: File? = null
+            if (mIncludePictures) {
+                if (iSlide == 0) {
+                    image = title
+                } else {
+                    image = ImageFiles.getFile(mStory, iSlide)
+                }
+            }
+            var audio = AudioFiles.getDramatization(mStory, iSlide)
+            //fallback to draft audio
+            if (!audio.exists()) {
+                audio = AudioFiles.getDraft(mStory, iSlide)
+            }
+            //fallback to LWC audio
+            if (!audio.exists()) {
+                //FIXME
+                //audio = AudioFiles.INSTANCE.getNarration(mStory, iSlide);
+            }
+            //error
+            if (!audio.exists()) {
+                error("Audio missing for slide " + (iSlide + 1))
+                return null
+            }
+
+            var soundtrack: File? = null
+            if (mIncludeBackgroundMusic) {
+                //FIXME
+                //soundtrack = AudioFiles.getSoundtrack(mStory, iSlide);
+                if (soundtrack == null) {
+                    //Try not to leave nulls in so null may be reserved for no soundtrack.
+                    soundtrack = lastSoundtrack
+                } else if (!soundtrack.exists()) {
+                    error("Soundtrack missing from template: " + soundtrack.name)
+                }
+
+                lastSoundtrack = soundtrack
+            }
+
+            var kbfx: KenBurnsEffect? = null
+            if (mIncludePictures && mIncludeKBFX && iSlide != 0) {
+                kbfx = KenBurnsSpec.getKenBurnsEffect(mStory, iSlide)
+            }
+
+            var text: String? = null
+            if (mIncludeText) {
+                text = TextFiles.getDramatizationText(mStory, iSlide)
+                text = if (text == "") null else text
+            }
+
+            pages.add(StoryPage(image, audio, 0, kbfx, text, soundtrack))
+            iSlide++
+        }
+
+        var copyrightImage: File? = null
+        if (mIncludePictures) {
+            copyrightImage = ImageFiles.getFile(mStory, ImageFiles.COPYRIGHT)
+        }
+        pages.add(StoryPage(copyrightImage, COPYRIGHT_SLIDE_US))
+
+        //TODO: add hymn
+
+        return pages.toTypedArray()
+    }
+
+    private fun watchProgress() {
+        val watcher = Thread(Runnable {
+            while (!mStoryMaker!!.isDone) {
+                val progress = mStoryMaker!!.progress
+                val audioProgress = mStoryMaker!!.audioProgress
+                val videoProgress = mStoryMaker!!.videoProgress
+                Log.i(TAG, "StoryMaker progress: " + MediaHelper.getDecimal(progress * 100) + "% "
+                        + "(audio " + MediaHelper.getDecimal(audioProgress * 100) + "% "
+                        + " and video " + MediaHelper.getDecimal(videoProgress * 100) + "%)")
+                try {
+                    Thread.sleep(1000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+
+            }
+        })
+
+        if (mLogProgress) {
+            watcher.start()
+        }
+    }
+
+    private fun error(message: String) {
+        if (mContext != null) {
+            Toast.makeText(mContext, message, Toast.LENGTH_LONG).show()
+        } else {
+            Log.e(TAG, message)
+        }
+    }
+
+    override fun close() {
+        if (mStoryMaker != null) {
+            mStoryMaker!!.close()
+        }
+    }
+
+    companion object {
+        private val TAG = "AutoStoryMaker"
+        //    private int mWidth = 1280;
+        //    private int mHeight = 720;
+
+        private val TITLE_FONT_SIZE = 20
+
+        private val SLIDE_CROSS_FADE_US: Long = 3000000
+        private val AUDIO_TRANSITION_US: Long = 500000
+
+        private val COPYRIGHT_SLIDE_US: Long = 2000000
+
+        // parameters for the video encoder
+        private val VIDEO_MIME_TYPE = "video/avc"    // H.264 Advanced Video Coding
+        private val VIDEO_FRAME_RATE = 30               // 30fps
+        private val VIDEO_IFRAME_INTERVAL = 1           // 1 second between I-frames
+
+        // using Kush Gauge for video bit rate
+        private val MOTION_FACTOR = 2                   // 1, 2, or 4
+        private val KUSH_GAUGE_CONSTANT = 0.07f
+
+        // parameters for the audio encoder
+        private val AUDIO_MIME_TYPE = "audio/mp4a-latm" //MediaFormat.MIMETYPE_AUDIO_AAC;
+        private val AUDIO_SAMPLE_RATE = 48000
+        private val AUDIO_CHANNEL_COUNT = 1
+        private val AUDIO_BIT_RATE = 64000
+    }
+}
