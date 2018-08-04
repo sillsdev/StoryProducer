63b9fc517aee7302137ed7cf1dd588f9417adf28
John Lambert <johnml1135@gmail.com>
2018-08-04 07:02:44 -0400

    Not the best icon, but otherwise, Issue #147 is resolved.

--- app/src/main/java/org/sil/storyproducer/controller/MainActivity.kt
+++ app/src/main/java/org/sil/storyproducer/controller/MainActivity.kt
@@ -1,14 +1,28 @@
 package org.sil.storyproducer.controller
 
+import android.app.AlertDialog
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.res.Configuration
+import android.graphics.drawable.ColorDrawable
 import android.net.ConnectivityManager
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.content.res.ResourcesCompat
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 import org.sil.storyproducer.R
 import org.sil.storyproducer.model.Story
@@ -17,12 +31,14 @@ import org.sil.storyproducer.tools.Network.VolleySingleton
 import org.sil.storyproducer.tools.StorySharedPreferences
 
 import org.sil.storyproducer.model.Workspace
+import org.sil.storyproducer.tools.DrawerItemClickListener
 
 
 import java.io.Serializable
 
 
 class MainActivity : AppCompatActivity(), Serializable {
+    private var mDrawerLayout: DrawerLayout? = null
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
@@ -44,11 +60,13 @@ class MainActivity : AppCompatActivity(), Serializable {
         StorySharedPreferences.init(applicationContext)
 
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        setupDrawer()
+
         supportFragmentManager.beginTransaction().add(R.id.fragment_container, StoryListFrag()).commit()
 
         this.applicationContext.registerReceiver(receiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+
+
 
         Workspace.updateStories(this)
     }
@@ -58,36 +76,6 @@ class MainActivity : AppCompatActivity(), Serializable {
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        when (id) {
-            R.id.menu_lang -> {
-            }
-            R.id.menu_registration -> {
-                val intent = Intent(this@MainActivity, RegistrationActivity::class.java)
-                startActivity(intent)
-            }
-            R.id.menu_license -> {
-                val dialog = AlertDialog.Builder(this)
-                        .setTitle(this.getString(R.string.license_title))
-                        .setMessage(this.getString(R.string.license_body))
-                        .setPositiveButton(this.getString(R.string.ok)) { dialog, id -> }.create()
-                dialog.show()
-            }
-        }//TODO remove this option.
-        return super.onOptionsItemSelected(item)
-    }
-
-    /**
-     * Upon language change, reload list of templates in that language
-     * The actual language change is done within the FileSystem class
-     */
-    private fun reloadStories() {
-        supportFragmentManager.beginTransaction().remove(supportFragmentManager.findFragmentById(R.id.fragment_container)).commit()
-        supportFragmentManager.beginTransaction().add(R.id.fragment_container, StoryListFrag()).commit()
-
-    }
-
     /**
      * move to the chosen story
      */
@@ -95,6 +83,72 @@ class MainActivity : AppCompatActivity(), Serializable {
         Workspace.activeStory = story
         val intent = Intent(this.applicationContext, Workspace.activePhase.getTheClass())
         startActivity(intent)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                mDrawerLayout!!.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+    /**
+     * initializes the items that the drawer needs
+     */
+    private fun setupDrawer() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeButtonEnabled(true)
+        mDrawerLayout = findViewById(R.id.drawer_layout)
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            mDrawerLayout!!.closeDrawers()
+
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+            val intent: Intent
+            //TODO add more options
+            when (menuItem.itemId) {
+                R.id.nav_workspace -> {
+                    intent = Intent(this, WorkspaceAndRegistrationActivity::class.java)
+                    this.startActivity(intent)
+                    this.finish()
+                }
+                R.id.nav_stories -> {
+                    intent = Intent(this.applicationContext, MainActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    this.startActivity(intent)
+                }
+                R.id.nav_registration -> {
+                    intent = Intent(this, RegistrationActivity::class.java)
+                    this.startActivity(intent)
+                    this.finish()
+                }
+                R.id.nav_license -> {
+                    val dialog = AlertDialog.Builder(this)
+                            .setTitle(this.getString(R.string.license_title))
+                            .setMessage(this.getString(R.string.license_body))
+                            .setPositiveButton(this.getString(R.string.ok)) { _, _ -> }.create()
+                    dialog.show()
+                }
+            }
+
+            true
+        }
+
+
     }
 }
 
