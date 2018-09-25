1f417f76b6d4a01d03f98f07e42e0b41315af7fe
Jonathan Taylor <jon.a.taylor@outlook.com>
2018-09-24 20:03:14 -0400

    SP-157: Remove unnecessary imports from example unit test.

--- app/src/test/java/org/sil/storyproducer/test/controller/TestSplashScreenActivity.java
+++ app/src/test/java/org/sil/storyproducer/test/controller/TestSplashScreenActivity.java
@@ -1,25 +1,17 @@
 package org.sil.storyproducer.test.controller;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Handler;
 
 import junit.framework.Assert;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.RuntimeEnvironment;
 import org.robolectric.Shadows;
 import org.robolectric.shadows.ShadowLooper;
-import org.sil.storyproducer.controller.MainActivity;
 import org.sil.storyproducer.controller.RegistrationActivity;
 import org.sil.storyproducer.controller.SplashScreenActivity;
-import static org.mockito.Mockito.*;
-import org.sil.storyproducer.model.Workspace;
 
 @RunWith(RobolectricTestRunner.class)
 public class TestSplashScreenActivity {
