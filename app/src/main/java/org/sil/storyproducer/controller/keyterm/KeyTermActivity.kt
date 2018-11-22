d6f2cb21dac89bb654faff54bdad6543d54f3926
aaroncannon <aaron.cannon@edusource.us>
2018-11-22 17:21:59 -0500

    Updated keyterm toolbar with close and help buttons

--- app/src/main/java/org/sil/storyproducer/controller/keyterm/KeyTermActivity.kt
+++ app/src/main/java/org/sil/storyproducer/controller/keyterm/KeyTermActivity.kt
@@ -3,11 +3,15 @@ package org.sil.storyproducer.controller.keyterm
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.NavUtils
 import android.support.v4.view.ViewPager
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ClickableSpan
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import org.sil.storyproducer.R
 import org.sil.storyproducer.model.Keyterm
@@ -27,10 +31,36 @@ class KeyTermActivity : AppCompatActivity() {
         viewPager.adapter = ViewPagerAdapter(supportFragmentManager, keyterm)
 
         //set action bar to have back button (android_manifest is where parent is set)
-        val toolBar: android.support.v7.widget.Toolbar = findViewById(R.id.keyterm_toolbar)
-        toolBar.title = keyterm.term
-        setSupportActionBar(toolBar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val toolbar: android.support.v7.widget.Toolbar = findViewById(R.id.keyterm_toolbar)
+        toolbar.title = keyterm.term
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_keyterm_view, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.closeKeyterm -> {
+                NavUtils.navigateUpFromSameTask(this)
+                true
+            }
+            R.id.helpButton -> {
+                val dialog = AlertDialog.Builder(this)
+                        .setTitle(getString(R.string.help))
+                        .setMessage(R.string.keyterm_help)
+                        .create()
+                dialog.show()
+                true
+            }
+            else -> {
+                NavUtils.navigateUpFromSameTask(this)
+                true
+            }
+        }
     }
 
     companion object {
