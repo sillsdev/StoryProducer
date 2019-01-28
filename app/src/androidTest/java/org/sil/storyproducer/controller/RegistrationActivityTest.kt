735811a9fe7d847b6698c9da122ca11e7b60d175
Jonathan Austin Taylor <jonathanataylor@cedarforest.cedarville.edu>
2019-01-28 15:24:45 -0500

    Go back to using a dummy workspace picker intent. Assumes app has been run once manually to select the workspace folder.

--- app/src/androidTest/java/org/sil/storyproducer/controller/RegistrationActivityTest.kt
+++ app/src/androidTest/java/org/sil/storyproducer/controller/RegistrationActivityTest.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.app.Instrumentation
 import android.content.Intent
 import android.net.Uri
-import android.os.Environment
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.scrollTo
@@ -15,14 +14,11 @@ import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
 import androidx.test.rule.GrantPermissionRule
 import org.hamcrest.CoreMatchers.containsString
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.io.File
-import java.util.regex.Pattern
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -41,16 +37,21 @@ class RegistrationActivityTest {
 
     @Test
     fun should_beAbleToSkipRegistration() {
-        Intents.init()
-        val expectedIntent = hasAction(Intent.ACTION_OPEN_DOCUMENT_TREE)
-        val workspaceFolderUri = Uri.parse(File(Environment.getExternalStorageDirectory().absolutePath, "SPWorkspace").toString())
-        val returnedIntent = Intent("idk", workspaceFolderUri)
-        Instrumentation.ActivityResult(Activity.RESULT_OK, returnedIntent)
-        intending(expectedIntent).respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, returnedIntent))
+        setUpDummyWorkspacePickerIntent()
         mActivityTestRule.launchActivity(null)
 
         onView(withText("Skip Registration")).perform(click())
         onView(withId(android.R.id.button1)).perform(scrollTo(), click())
         onView(withText(containsString("Lost Coin"))).check(matches(isDisplayed()))
+    }
+
+    private fun setUpDummyWorkspacePickerIntent() {
+        Intents.init()
+        val expectedIntent = hasAction(Intent.ACTION_OPEN_DOCUMENT_TREE)
+        val returnedIntent = Intent()
+        // An empty Uri will cause Workspace.setupWorkspacePass to silently fail.
+        // This allows the test to proceed with a manually set workspace directory.
+        returnedIntent.setData(Uri.EMPTY)
+        intending(expectedIntent).respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, returnedIntent))
     }
 }
