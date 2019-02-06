bce5fac0cc725c4e796ad255ffa14a05a1ab5312
Jonathan Austin Taylor <jonathanataylor@cedarforest.cedarville.edu>
2019-02-06 15:58:58 -0500

    Move duplicated dummy intent setup code into a new file called IntentMocker.

--- app/src/androidTest/java/org/sil/storyproducer/androidtest/happypath/RegistrationActivityTest.kt
+++ app/src/androidTest/java/org/sil/storyproducer/androidtest/happypath/RegistrationActivityTest.kt
@@ -1,16 +1,9 @@
 package org.sil.storyproducer.androidtest.happypath
 
-import android.app.Activity
-import android.app.Instrumentation
-import android.content.Intent
-import android.net.Uri
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.Intents.intending
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -19,6 +12,8 @@ import org.hamcrest.CoreMatchers.containsString
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.sil.storyproducer.androidtest.utilities.IntentMocker.setUpDummyWorkspacePickerIntent
+import org.sil.storyproducer.androidtest.utilities.IntentMocker.tearDownDummyWorkspacePickerIntent
 import org.sil.storyproducer.controller.RegistrationActivity
 
 @LargeTest
@@ -45,19 +40,6 @@ class RegistrationActivityTest {
         onView(withText("Skip Registration")).perform(click())
         onView(withId(android.R.id.button1)).perform(scrollTo(), click())
         onView(withText(containsString("Lost Coin"))).check(matches(isDisplayed()))
-    }
-
-    private fun setUpDummyWorkspacePickerIntent() {
-        Intents.init()
-        val expectedIntent = hasAction(Intent.ACTION_OPEN_DOCUMENT_TREE)
-        val returnedIntent = Intent()
-        // An empty Uri will cause Workspace.setupWorkspacePass to silently fail.
-        // This allows the test to proceed with a manually set workspace directory.
-        returnedIntent.data = Uri.EMPTY
-        intending(expectedIntent).respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, returnedIntent))
-    }
-    private fun tearDownDummyWorkspacePickerIntent() {
-        Intents.release()
     }
 
 }
--- app/src/androidTest/java/org/sil/storyproducer/androidtest/happypath/TranslatePhaseTest.kt
+++ app/src/androidTest/java/org/sil/storyproducer/androidtest/happypath/TranslatePhaseTest.kt
@@ -1,17 +1,11 @@
 package org.sil.storyproducer.androidtest.happypath
 
 import android.app.Activity
-import android.app.Instrumentation
-import android.content.Intent
-import android.net.Uri
 import android.preference.PreferenceManager
 import android.support.v7.widget.AppCompatSeekBar
 import android.support.v7.widget.AppCompatTextView
 import android.view.View
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.Intents.intending
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -29,6 +23,8 @@ import androidx.test.espresso.UiController
 import org.hamcrest.Matcher
 import org.junit.*
 import org.sil.storyproducer.R
+import org.sil.storyproducer.androidtest.utilities.IntentMocker.setUpDummyWorkspacePickerIntent
+import org.sil.storyproducer.androidtest.utilities.IntentMocker.tearDownDummyWorkspacePickerIntent
 import org.sil.storyproducer.controller.RegistrationActivity
 
 
@@ -148,22 +144,8 @@ class TranslatePhaseTest {
         onView(withText("Skip Registration")).perform(click())
         onView(withId(android.R.id.button1)).perform(scrollTo(), click())
         onView(withText(containsString("Lost Coin"))).perform(scrollTo(), click())
-        onView(withId(org.sil.storyproducer.R.id.toolbar)).perform(click())
+        onView(withId(R.id.toolbar)).perform(click())
         onData(allOf(`is`(instanceOf(String::class.java)), `is`("Translate"))).perform(click())
-    }
-
-    private fun setUpDummyWorkspacePickerIntent() {
-        Intents.init()
-        val expectedIntent = hasAction(Intent.ACTION_OPEN_DOCUMENT_TREE)
-        val returnedIntent = Intent()
-        // An empty Uri will cause Workspace.setupWorkspacePass to silently fail.
-        // This allows the test to proceed with a manually set workspace directory.
-        returnedIntent.setData(Uri.EMPTY)
-        intending(expectedIntent).respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, returnedIntent))
-    }
-
-    private fun tearDownDummyWorkspacePickerIntent() {
-        Intents.release()
     }
 
     // See https://stackoverflow.com/questions/24517291/get-current-activity-in-espresso-android
