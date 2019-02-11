7e18ee8af978ad8b7ef94dfcbd5ab82f0babfbc0
Jonathan Austin Taylor <jonathanataylor@cedarforest.cedarville.edu>
2019-02-11 15:42:37 -0500

    Move boilerplate ui navigation to a PhaseNavigator utility. Move common function for obtaining current activity into ActivityAccessor utility. Add a base test class for tests that navigate to a phase.

--- app/src/androidTest/java/org/sil/storyproducer/androidtest/happypath/TranslatePhaseTest.kt
+++ app/src/androidTest/java/org/sil/storyproducer/androidtest/happypath/TranslatePhaseTest.kt
@@ -1,49 +1,32 @@
 package org.sil.storyproducer.androidtest.happypath
 
-import android.app.Activity
 import android.preference.PreferenceManager
 import android.support.v7.widget.AppCompatSeekBar
 import android.support.v7.widget.AppCompatTextView
-import android.view.View
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.rule.GrantPermissionRule
 import org.hamcrest.CoreMatchers.*
 import org.junit.runner.RunWith
-import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import org.junit.runners.MethodSorters
 import java.lang.Integer.parseInt
-import androidx.test.espresso.ViewAction
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.UiController
-import org.hamcrest.Matcher
 import org.junit.*
-import org.sil.storyproducer.androidtest.utilities.IntentMocker.setUpDummyWorkspacePickerIntent
-import org.sil.storyproducer.androidtest.utilities.IntentMocker.tearDownDummyWorkspacePickerIntent
-import org.sil.storyproducer.androidtest.utilities.PermissionsGranter
-import org.sil.storyproducer.controller.RegistrationActivity
+import org.sil.storyproducer.R
+import org.sil.storyproducer.androidtest.utilities.ActivityAccessor
+import org.sil.storyproducer.androidtest.utilities.PhaseNavigator
 
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-class TranslatePhaseTest {
+class TranslatePhaseTest : PhaseTestBase() {
 
-    @Rule
-    @JvmField
-    val mActivityTestRule = androidx.test.rule.ActivityTestRule(RegistrationActivity::class.java, false, false)
-
-    @Rule
-    @JvmField
-    var mGrantPermissionRule: GrantPermissionRule = PermissionsGranter.grantStoryProducerPermissions()
-
-    @Before
-    fun setUp() {
-        navigateToTranslatePhase()
+    override fun navigateToPhase() {
+        PhaseNavigator.navigateFromRegistrationScreenToTranslatePhase()
     }
 
     @Test
@@ -81,12 +64,12 @@ class TranslatePhaseTest {
     }
 
     private fun enableCustomAnimations() {
-        val preferencesEditor = PreferenceManager.getDefaultSharedPreferences(getActivity()).edit()
+        val preferencesEditor = PreferenceManager.getDefaultSharedPreferences(ActivityAccessor.getCurrentActivity()).edit()
         preferencesEditor.remove(mActivityTestRule.activity.resources.getString(org.sil.storyproducer.R.string.recording_toolbar_disable_animation))
     }
 
     private fun disableCustomAnimations() {
-        val preferencesEditor = PreferenceManager.getDefaultSharedPreferences(getActivity()).edit()
+        val preferencesEditor = PreferenceManager.getDefaultSharedPreferences(ActivityAccessor.getCurrentActivity()).edit()
         preferencesEditor.putBoolean(mActivityTestRule.activity.resources.getString(org.sil.storyproducer.R.string.recording_toolbar_disable_animation), true)
         preferencesEditor.commit()
     }
@@ -104,7 +87,7 @@ class TranslatePhaseTest {
     }
 
     private fun findCurrentSlideNumber(): Int {
-        val slideNumberTextView = getActivity()?.findViewById<AppCompatTextView>(org.sil.storyproducer.R.id.slide_number_text)
+        val slideNumberTextView = ActivityAccessor.getCurrentActivity()?.findViewById<AppCompatTextView>(org.sil.storyproducer.R.id.slide_number_text)
         return parseInt(slideNumberTextView!!.text.toString())
     }
 
@@ -121,7 +104,7 @@ class TranslatePhaseTest {
     }
 
     private fun getCurrentSlideAudioProgress(): Int {
-        val progressBar = getActivity()?.findViewById<AppCompatSeekBar>(org.sil.storyproducer.R.id.videoSeekBar)
+        val progressBar = ActivityAccessor.getCurrentActivity()?.findViewById<AppCompatSeekBar>(org.sil.storyproducer.R.id.videoSeekBar)
         return progressBar!!.progress
     }
 
@@ -133,37 +116,4 @@ class TranslatePhaseTest {
         Thread.sleep(250)
     }
 
-    private fun navigateToTranslatePhase() {
-        setUpDummyWorkspacePickerIntent()
-        mActivityTestRule.launchActivity(null)
-        tearDownDummyWorkspacePickerIntent()
-
-        onView(withText("Skip Registration")).perform(click())
-        onView(withId(android.R.id.button1)).perform(scrollTo(), click())
-        onView(withText(containsString("Lost Coin"))).perform(scrollTo(), click())
-        onView(withId(R.id.toolbar)).perform(click())
-        onData(allOf(`is`(instanceOf(String::class.java)), `is`("Translate"))).perform(click())
-    }
-
-    // See https://stackoverflow.com/questions/24517291/get-current-activity-in-espresso-android
-    private fun getActivity(): Activity? {
-        val currentActivity = arrayOfNulls<Activity>(1)
-        onView(allOf(withId(android.R.id.content), isDisplayed())).perform(object : ViewAction {
-            override fun getConstraints(): Matcher<View> {
-                return isAssignableFrom(View::class.java)
-            }
-
-            override fun getDescription(): String {
-                return "getting text from a TextView"
-            }
-
-            override fun perform(uiController: UiController, view: View) {
-                if (view.context is Activity) {
-                    val activity1 = view.context as Activity
-                    currentActivity[0] = activity1
-                }
-            }
-        })
-        return currentActivity[0]
-    }
 }
