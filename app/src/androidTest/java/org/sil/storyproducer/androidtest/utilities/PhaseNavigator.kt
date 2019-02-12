994e35049ff3d3fe4441c39180c6026e9e6c6b18
Ben Ratkey <benratkey@gmail.com>
2019-02-11 19:20:04 -0500

    SP-258: revisions based on code review comments

--- app/src/androidTest/java/org/sil/storyproducer/androidtest/utilities/PhaseNavigator.kt
+++ app/src/androidTest/java/org/sil/storyproducer/androidtest/utilities/PhaseNavigator.kt
@@ -14,6 +14,13 @@ object PhaseNavigator {
         Espresso.onData(CoreMatchers.allOf(CoreMatchers.`is`(CoreMatchers.instanceOf(String::class.java)), CoreMatchers.`is`("Translate"))).perform(ViewActions.click())
     }
 
+    fun navigateFromRegistrationScreenToLearnPhase() {
+        skipRegistration()
+        Espresso.onView(ViewMatchers.withText(CoreMatchers.containsString("Lost Coin"))).perform(ViewActions.scrollTo(), ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(org.sil.storyproducer.R.id.toolbar)).perform(ViewActions.click())
+        Espresso.onData(CoreMatchers.allOf(CoreMatchers.`is`(CoreMatchers.instanceOf(String::class.java)), CoreMatchers.`is`("Learn"))).perform(ViewActions.click())
+    }
+
     private fun skipRegistration() {
         Espresso.onView(ViewMatchers.withText("Skip Registration")).perform(ViewActions.click())
         Espresso.onView(ViewMatchers.withId(R.id.button1)).perform(ViewActions.scrollTo(), ViewActions.click())
