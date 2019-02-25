1a0458b00db15d3d2e7f7031b0b0ef2c643d056e
Ben Ratkey <benratkey@gmail.com>
2019-02-25 14:22:10 -0500

    SP-262 work on toggling approved state of slides

--- app/src/androidTest/java/org/sil/storyproducer/androidtest/happypath/AccuracyCheckPhaseTest.kt
+++ app/src/androidTest/java/org/sil/storyproducer/androidtest/happypath/AccuracyCheckPhaseTest.kt
@@ -2,9 +2,15 @@ package org.sil.storyproducer.androidtest.happypath
 
 import android.preference.PreferenceManager
 import android.support.v7.widget.AppCompatSeekBar
+import android.support.v7.widget.AppCompatTextView
+import android.view.View
 import android.widget.ImageButton
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -14,12 +20,18 @@ import org.junit.runner.RunWith
 import org.junit.FixMethodOrder
 import org.junit.runners.MethodSorters
 import junit.framework.Assert.assertTrue
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
+import org.sil.storyproducer.R
 import org.sil.storyproducer.androidtest.utilities.ActivityAccessor
+import org.sil.storyproducer.androidtest.utilities.AnimationsToggler
 import org.sil.storyproducer.androidtest.utilities.PhaseNavigator
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class AccuracyCheckPhaseTest : PhaseTestBase() {
+    val durationToRecordTranslatedClip: Long = 1000
+    val durationToPlayTranslatedClip: Long = 100
 
     override fun navigateToPhase() {
         PhaseNavigator.navigateFromRegistrationScreenToAccuracyCheckPhase()
@@ -27,17 +39,43 @@ class AccuracyCheckPhaseTest : PhaseTestBase() {
 
     @Test
     fun shouldBeAbleToPlayRecordedAudioForSpecificSlide() {
-        //TODO
+        makeSureAnAudioClipIsAvailable()
+
+        val originalProgress = getCurrentSlideAudioProgress()
+        pressPlayPauseButton()
+        Thread.sleep(durationToPlayTranslatedClip)
+        pressPlayPauseButton()
+        val progressAfterPausing = getCurrentSlideAudioProgress()
+        Assert.assertTrue("Expected progress bar to increase in position.", progressAfterPausing > originalProgress)
     }
 
     @Test
     fun shouldBeAbleToToggleApprovedState() {
+        Thread.sleep(1000)
+        pressCheckmarkButton()
+        Thread.sleep(1000)
+        pressCheckmarkButton()
+        Thread.sleep(1000)
+        pressCheckmarkButton()
+        Thread.sleep(1000)
+        pressCheckmarkButton()
+        Thread.sleep(1000)
         //TODO
     }
 
     @Test
     fun shouldBeAbleToSwipeBetweenSlides() {
-        //TODO
+        val originalSlideNumber = findCurrentSlideNumber()
+        var nextSlideNumber = originalSlideNumber + 1
+        expectToBeOnSlide(originalSlideNumber)
+        // swipe to the left
+        swipeLeftOnSlide()
+        Thread.sleep(50)
+        expectToBeOnSlide(nextSlideNumber)
+        // swipe to the right
+        swipeRightOnSlide()
+        Thread.sleep(50)
+        expectToBeOnSlide(originalSlideNumber)
     }
 
     @Test
@@ -45,15 +83,64 @@ class AccuracyCheckPhaseTest : PhaseTestBase() {
         //TODO
     }
 
-    private fun enableCustomAnimations() {
-        val preferencesEditor = PreferenceManager.getDefaultSharedPreferences(ActivityAccessor.getCurrentActivity()).edit()
-        preferencesEditor.remove(mActivityTestRule.activity.resources.getString(org.sil.storyproducer.R.string.recording_toolbar_disable_animation))
+    private fun makeSureAnAudioClipIsAvailable() {
+        selectPhase("Translate")
+        if (!areThereAnyAudioClipsOnThisSlide()) {
+            recordAnAudioTranslationClip()
+        }
+        selectPhase("Accuracy Check")
     }
 
-    private fun disableCustomAnimations() {
-        val preferencesEditor = PreferenceManager.getDefaultSharedPreferences(ActivityAccessor.getCurrentActivity()).edit()
-        preferencesEditor.putBoolean(mActivityTestRule.activity.resources.getString(org.sil.storyproducer.R.string.recording_toolbar_disable_animation), true)
-        preferencesEditor.commit()
+    private fun selectPhase(phaseTitle: String) {
+        Espresso.onView(ViewMatchers.withId(org.sil.storyproducer.R.id.toolbar)).perform(ViewActions.click())
+        Espresso.onData(CoreMatchers.allOf(CoreMatchers.`is`(CoreMatchers.instanceOf(String::class.java)), CoreMatchers.`is`(phaseTitle))).perform(ViewActions.click())
+    }
+
+    private fun areThereAnyAudioClipsOnThisSlide(): Boolean {
+        val showRecordingsListButton = ActivityAccessor.getCurrentActivity()?.findViewById<ImageButton>(org.sil.storyproducer.R.id.list_recordings_button)
+        return showRecordingsListButton?.visibility != View.INVISIBLE
+    }
+
+    private fun recordAnAudioTranslationClip() {
+        AnimationsToggler.disableCustomAnimations()
+        pressMicButton()
+        Thread.sleep(durationToRecordTranslatedClip)
+        pressMicButton()
+        AnimationsToggler.enableCustomAnimations()
+    }
+
+    private fun pressMicButton() {
+        onView(allOf(withId(R.id.start_recording_button), isDisplayed())).perform(click())
+    }
+
+    private fun pressPlayPauseButton() {
+        Espresso.onView(CoreMatchers.allOf(ViewMatchers.withId(R.id.fragment_reference_audio_button), isDisplayed())).perform(click())
+    }
+
+    private fun getCurrentSlideAudioProgress(): Int {
+        val progressBar = ActivityAccessor.getCurrentActivity()?.findViewById<AppCompatSeekBar>(org.sil.storyproducer.R.id.videoSeekBar)
+        return progressBar!!.progress
+    }
+
+    private fun pressCheckmarkButton() {
+        onView(allOf(withId(R.id.concheck_checkmark_button), isDisplayed())).perform(click())
+    }
+
+    private fun findCurrentSlideNumber(): Int {
+        val slideNumberTextView = ActivityAccessor.getCurrentActivity()?.findViewById<AppCompatTextView>(org.sil.storyproducer.R.id.slide_number_text)
+        return Integer.parseInt(slideNumberTextView!!.text.toString())
+    }
+
+    private fun expectToBeOnSlide(originalSlideNumber: Int) {
+        Espresso.onView(CoreMatchers.allOf(ViewMatchers.withId(R.id.slide_number_text), ViewMatchers.withText(originalSlideNumber.toString()))).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    private fun swipeLeftOnSlide() {
+        Espresso.onView(allOf(ViewMatchers.withId(R.id.phase_frame))).perform(swipeLeft())
+    }
+
+    private fun swipeRightOnSlide() {
+        Espresso.onView(allOf(ViewMatchers.withId(R.id.phase_frame))).perform(swipeRight())
     }
 
 }
--- app/src/main/java/org/sil/storyproducer/controller/consultant/ConsultantCheckFrag.kt
+++ app/src/main/java/org/sil/storyproducer/controller/consultant/ConsultantCheckFrag.kt
@@ -25,8 +25,13 @@ import org.sil.storyproducer.model.Workspace
 class ConsultantCheckFrag : SlidePhaseFrag() {
 
     var logDialog: AlertDialog? = null
+    var greenCheckmark: VectorDrawableCompat ?= null
+    var grayCheckmark: VectorDrawableCompat ?= null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        greenCheckmark = VectorDrawableCompat.create(resources, R.drawable.ic_checkmark_green, null)
+        grayCheckmark = VectorDrawableCompat.create(resources, R.drawable.ic_checkmark_gray, null)
 
         // The last two arguments ensure LayoutParams are inflated
         // properly.
@@ -66,13 +71,10 @@ class ConsultantCheckFrag : SlidePhaseFrag() {
      * @param button the check button
      */
     private fun setCheckmarkButton(button: ImageButton) {
-        //TODO replace T/f with storing MD5 or SHA1 of the draft audio.
         if (Workspace.activeStory.slides[slideNum].isChecked) {
-            //TODO: use non-deprecated method; currently used to support older devices
-            button.background = VectorDrawableCompat.create(resources, R.drawable.ic_checkmark_green, null)
+            button.background = greenCheckmark
         } else {
-            //TODO: use non-deprecated method; currently used to support older devices
-            button.background = VectorDrawableCompat.create(resources, R.drawable.ic_checkmark_gray, null)
+            button.background = grayCheckmark
         }
         button.setOnClickListener(View.OnClickListener {
             if (Workspace.activeStory.isApproved) {
@@ -80,10 +82,10 @@ class ConsultantCheckFrag : SlidePhaseFrag() {
                 return@OnClickListener
             }
             if (Workspace.activeStory.slides[slideNum].isChecked) {
-                button.background = VectorDrawableCompat.create(resources, R.drawable.ic_checkmark_gray, null)
+                button.background = grayCheckmark
                 Workspace.activeStory.slides[slideNum].isChecked = false
             } else {
-                button.background = VectorDrawableCompat.create(resources, R.drawable.ic_checkmark_green, null)
+                button.background = greenCheckmark
                 Workspace.activeStory.slides[slideNum].isChecked = true
                 if (checkAllMarked()) {
                     showConsultantPasswordDialog()
