7d34f35bf753bb99ecf6ed4a9854d029b88a5bbd
Aaron Cannon <aarontcannon@cedarville.edu>
2019-04-15 15:20:24 -0400

    Merge branch '2.4_Keyterm-Tracker' into SP-371_Refactor-and-test-Recording-Toolbar
    # Conflicts:
    #	app/src/main/java/org/sil/storyproducer/tools/toolbar/RecordingToolbar.kt

--- app/src/main/java/org/sil/storyproducer/controller/dramatization/DramatizationRecordingToolbar.kt
+++ app/src/main/java/org/sil/storyproducer/controller/dramatization/DramatizationRecordingToolbar.kt
@@ -1,7 +1,6 @@
 package org.sil.storyproducer.controller.dramatization
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.ImageButton
 import com.crashlytics.android.Crashlytics
