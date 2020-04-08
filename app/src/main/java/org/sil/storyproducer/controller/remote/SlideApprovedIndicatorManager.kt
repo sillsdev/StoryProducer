d09ed24b5eccf9125c002f419ada4e9e2c2dbc29
Philip White <philipwhite@cedarville.edu>
2020-04-08 15:33:08 -0400

    Approved story when all slides get approved remotely

--- app/src/main/java/org/sil/storyproducer/controller/remote/SlideApprovedIndicatorManager.kt
+++ app/src/main/java/org/sil/storyproducer/controller/remote/SlideApprovedIndicatorManager.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import android.support.graphics.drawable.VectorDrawableCompat
 import android.view.*
 import android.widget.*
+import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.sil.storyproducer.R
 import org.sil.storyproducer.model.Slide
+import org.sil.storyproducer.model.SlideType
 import org.sil.storyproducer.model.Workspace
 import org.sil.storyproducer.model.messaging.Approval
 
@@ -36,6 +38,9 @@ class ApprovalIndicatorManager(
     }
 
     public fun start() {
+        // TODO @pwhite: Make only one instance of these run at a time, so make
+        // sure start and stop happen upon visibility changes.
+
         approvedIndicator.background = if (slide.isApproved) {
             greenCheckmark
         } else {
@@ -48,6 +53,20 @@ class ApprovalIndicatorManager(
                 if (approval.slideNumber == slideNumber && approval.storyId == Workspace.activeStory.remoteId) {
                     approvedIndicator.background = if (approval.approvalStatus) { greenCheckmark } else { grayCheckmark }
                 }
+
+                // Approve story if all slides in the story have been approved.
+                var allApproved = true
+                var story = Workspace.activeStory
+                for (slide in story.slides) {
+                    if ((slide.slideType == SlideType.FRONTCOVER ||
+                         slide.slideType == SlideType.NUMBEREDPAGE ||
+                         slide.slideType == SlideType.LOCALSONG) && 
+                        !slide.isApproved) {
+
+                        allApproved = false
+                    }
+                }
+                story.isApproved = allApproved
             }
         }
     }
