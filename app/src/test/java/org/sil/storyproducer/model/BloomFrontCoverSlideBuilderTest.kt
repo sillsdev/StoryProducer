2ef87d2be0144f6637bc162bfc6ac38876e1fc05
Chad Dalton <mungomash@gmail.com>
2020-06-14 14:43:56 -0400

    fix broken BloomFrontCoverSlideBuilderTest

--- app/src/test/java/org/sil/storyproducer/model/BloomFrontCoverSlideBuilderTest.kt
+++ app/src/test/java/org/sil/storyproducer/model/BloomFrontCoverSlideBuilderTest.kt
@@ -1,8 +1,12 @@
 package org.sil.storyproducer.model
 
-import org.junit.Assert
+import android.content.Context
+import androidx.documentfile.provider.DocumentFile
+import org.jsoup.Jsoup
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
@@ -10,118 +14,48 @@ class BloomFrontCoverSlideBuilderTest {
 
     val builder = BloomFrontCoverSlideBuilder()
 
-    /*
+    @Mock lateinit var context: Context
+    @Mock lateinit var file: DocumentFile
+
+
     @Test
     fun testBuildFrontCoverTitleIdeas() {
-        val slide = Slide().apply {
-            content = "Video Title Slide Content\n" +
-                    "Graphic=gray\n" +
-                    "ScriptureReference=Matt 14.22-33; Mk 6.45-50; John 6.15-20\n" +
-                    "TitleIdeasHeading=Title ideas:\n" +
-                    "TitleIdea=\"Jesus walked on the water.\"\n" +
-                    "TitleIdea=Jesus stopped a storm.\n" +
-                    "TitleIdea=Jesus’s disciples understand who he is.\n" +
-                    "TitleIdea='Is Jesus God?'"
-        }
+        val text = """
+            <body>
+                <div id="bloomDataDiv">
+                    <div lang="*" data-book="contentLanguage1">
+                        en
+                    </div>
+                    <div lang="en" data-book="spTitleIdeasHeading">
+                        Title ideas:
+                    </div>
+                    <div lang="en" data-book="spTitleIdea1">
+                        <p><span>Jesus walked on the water.</span></p>
+                    </div>
+                    <div lang="en" data-book="spTitleIdea2">
+                        <p><span>Jesus stopped a storm.</span></p>
+                    </div>
+            
+                    <div lang="en" data-book="spTitleIdea3">
+                        <p><span>Jesus’s disciples understand who he is.</span></p>
+                    </div>
+                    <div lang="en" data-book="spTitleIdea4">
+                        <p><span>Is Jesus God?</span></p>
+                    </div>
+                </div>
+                <div class="outsideFrontCover" />
+            </body>
+        """.trimIndent()
 
-        Assert.assertEquals(
+        assertEquals(
                 "Title ideas:\n" +
                         "Jesus walked on the water.\n" +
                         "Jesus stopped a storm.\n" +
                         "Jesus’s disciples understand who he is.\n" +
                         "Is Jesus God?",
-                builder.buildTitleIdeas(FrontCoverContent(slide.content, "", ""))
+                builder.build(context, file, Jsoup.parse(text))?.content
         )
     }
-
-    @Test
-    fun testBuildFrontCoverTitleIdeas_WhenNumbered() {
-        val slide = Slide().apply {
-            content = "Video Title Slide Content\n" +
-                    "Graphic=gray\n" +
-                    "ScriptureReference=Matt 14.22-33; Mk 6.45-50; John 6.15-20\n" +
-                    "TitleIdeasHeading=Title ideas:\n" +
-                    "TitleIdea1=Jesus walked on the water.\n" +
-                    "TitleIdea2=Jesus stopped a storm.\n" +
-                    "TitleIdea3=Jesus’s disciples understand who he is.\n" +
-                    "TitleIdea4=Is Jesus God?"
-        }
-
-        Assert.assertEquals(
-                "Title ideas:\n" +
-                        "Jesus walked on the water.\n" +
-                        "Jesus stopped a storm.\n" +
-                        "Jesus’s disciples understand who he is.\n" +
-                        "Is Jesus God?",
-                builder.buildTitleIdeas(FrontCoverContent(slide.content, "", ""))
-        )
-    }
-
-    @Test
-    fun testBuildFrontCoverTitleIdeas_WhenNumberedOutOfOrder() {
-        val slide = Slide().apply {
-            content = "Video Title Slide Content\n" +
-                    "Graphic=gray\n" +
-                    "ScriptureReference=Matt 14.22-33; Mk 6.45-50; John 6.15-20\n" +
-                    "TitleIdeasHeading=Title ideas:\n" +
-                    "TitleIdea3=Jesus walked on the water.\n" +
-                    "TitleIdea2=Jesus stopped a storm.\n" +
-                    "TitleIdea1=Jesus’s disciples understand who he is.\n" +
-                    "TitleIdea4=Is Jesus God?"
-        }
-
-        Assert.assertEquals(
-                "Title ideas:\n" +
-                        "Jesus walked on the water.\n" +
-                        "Jesus stopped a storm.\n" +
-                        "Jesus’s disciples understand who he is.\n" +
-                        "Is Jesus God?",
-                builder.buildTitleIdeas(FrontCoverContent(slide.content, "", ""))
-        )
-    }
-
-    @Test
-    fun testBuildFrontCoverTitleIdeas_WhenSpaceAroundEquals() {
-        val slide = Slide().apply {
-            content = "Video Title Slide Content\n" +
-                    "Graphic= gray\n" +
-                    "ScriptureReference =Genesis 1 &amp; 2\n" +
-                    "TitleIdeasHeading= Title ideas:\n" +
-                    "TitleIdea=The true story / account about how God created / made the earth and the first people.\n" +
-                    "TitleIdea2=God created everything\n" +
-                    "TitleIdea     = The beginning"
-        }
-
-        Assert.assertEquals(
-                "Title ideas:\n" +
-                        "The true story / account about how God created / made the earth and the first people.\n" +
-                        "God created everything\n" +
-                        "The beginning",
-                builder.buildTitleIdeas(FrontCoverContent(slide.content, "", ""))
-        )
-    }
-
-    @Test
-    fun testBuildFrontCoverTitleIdeas_WhenContainsZeroWidthCharacter() {
-        val slide = Slide().apply {
-            content = "Video Title Slide Content\n" +
-                    "Graphic= gray\n" +
-                    "ScriptureReference =Genesis 1 &amp; 2\n" +
-                    "TitleIdeasHeading= Title ideas:\n" +
-                    "\u200CTitleIdea=The true story / account about how God created / made the earth and the first people.\n" +
-                    "TitleIdea2=God created everything\n" +
-                    "TitleIdea = The beginning\u200C"
-        }
-
-        Assert.assertEquals(
-                "Title ideas:\n" +
-                        "The true story / account about how God created / made the earth and the first people.\n" +
-                        "God created everything\n" +
-                        "The beginning",
-                builder.buildTitleIdeas(FrontCoverContent(slide.content, "", ""))
-        )
-    }
-     */
 
 
 }
