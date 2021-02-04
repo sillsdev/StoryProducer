13b7f1e57930b42e1e1566f112e7a9356f28e7c2
Jake Allinson <jakeallinson03@gmail.com>
2021-02-04 11:56:56 -0500

    changes welcome dialog to a TextView

--- app/src/main/java/org/sil/storyproducer/activity/WorkspaceDialogUpdateActivity.kt
+++ app/src/main/java/org/sil/storyproducer/activity/WorkspaceDialogUpdateActivity.kt
@@ -5,7 +5,14 @@ import android.os.Build
 import android.os.Bundle
 import android.text.Html
 import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import org.sil.storyproducer.R
+
 
 class WorkspaceDialogUpdateActivity : BaseActivity() {
 
@@ -15,10 +22,27 @@ class WorkspaceDialogUpdateActivity : BaseActivity() {
     }
 
     private fun showWelcomeDialog() {
+        // Issue #541: welcome screen is now created using a layout View
+        val view = LayoutInflater.from(this).inflate(R.layout.dialog_welcome_screen, null) as ViewGroup
+        val mainTextView = view.findViewById<TextView>(R.id.main_textview)
+        mainTextView.setText(buildMessage())
+        mainTextView.movementMethod = LinkMovementMethod.getInstance()
+
+        // demo button not available, show error as of 3.0.4beta
+        val demoTextView = view.findViewById<TextView>(R.id.demo_textview);
+        demoTextView.setOnClickListener {
+            val errorToast = Toast.makeText(this, R.string.welcome_screen_demo_error, Toast.LENGTH_SHORT)
+            errorToast.show()
+        }
+
+        val selectTemplatesButton = view.findViewById<Button>(R.id.select_templates_button);
+        selectTemplatesButton.setOnClickListener {
+            selectTemplatesFolder()
+        }
+
         AlertDialog.Builder(this)
                 .setTitle(buildTitle())
-                .setMessage(buildMessage())
-                .setPositiveButton(R.string.update_workspace) { _, _ -> selectTemplatesFolder() }
+                .setView(view)
                 .setCancelable(false)
                 .create()
                 .show()
