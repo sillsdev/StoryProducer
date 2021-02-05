1e3da2a7d01aa876d0420852a65f10762242a423
Jake Allinson <38381688+jakedallinson@users.noreply.github.com>
2021-02-05 18:43:43 -0500

    Issue #541 - Revise Welcome Screen (#543)
    * updates welcome screen strings and removes add demo functions
    
    * adds subtext for parenthetical and indentations
    
    * updates the dialog for selecting templates folder from main menu
    
    * changes welcome dialog to a TextView
    
    * adds additional dialog box call after welcome screen
    
    * string update to select folder dialog
    
    by: Jake Allinson <jakeallinson03@gmail.com>

--- app/src/main/java/org/sil/storyproducer/activity/WorkspaceDialogUpdateActivity.kt
+++ app/src/main/java/org/sil/storyproducer/activity/WorkspaceDialogUpdateActivity.kt
@@ -5,7 +5,14 @@ import android.os.Build
 import android.os.Bundle
 import android.text.Html
 import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import org.sil.storyproducer.R
+
 
 class WorkspaceDialogUpdateActivity : BaseActivity() {
 
@@ -15,21 +22,28 @@ class WorkspaceDialogUpdateActivity : BaseActivity() {
     }
 
     private fun showWelcomeDialog() {
-        AlertDialog.Builder(this)
-                .setTitle(buildTitle())
-                .setMessage(buildMessage())
-                .setPositiveButton(R.string.use_internal_demo) { _, _ -> showCreateAndSelectFolderDialog() }
-                .setNegativeButton(R.string.update_workspace) { _, _ -> selectTemplatesFolder() }
-                .setCancelable(false)
-                .create()
-                .show()
-    }
+        // Issue #541: welcome screen is now created using a layout View
+        val view = LayoutInflater.from(this).inflate(R.layout.dialog_welcome_screen, null) as ViewGroup
+        val mainTextView = view.findViewById<TextView>(R.id.main_textview)
+        mainTextView.setText(buildMessage())
+        mainTextView.movementMethod = LinkMovementMethod.getInstance()
 
-    private fun showCreateAndSelectFolderDialog() {
+        // demo button not available, show error as of 3.0.4beta
+        val demoTextView = view.findViewById<TextView>(R.id.demo_textview);
+        demoTextView.setOnClickListener {
+            val errorToast = Toast.makeText(this, R.string.welcome_screen_demo_error, Toast.LENGTH_SHORT)
+            errorToast.show()
+        }
+
+        val selectTemplatesButton = view.findViewById<Button>(R.id.select_templates_button);
+        selectTemplatesButton.setOnClickListener {
+            //selectTemplatesFolder()
+            showSelectTemplatesFolderDialog()
+        }
+
         AlertDialog.Builder(this)
                 .setTitle(buildTitle())
-                .setMessage(R.string.please_create_and_then_select_folder)
-                .setPositiveButton(R.string.ok) { _, _ -> selectTemplatesFolderAndAddDemo() }
+                .setView(view)
                 .setCancelable(false)
                 .create()
                 .show()
@@ -44,7 +58,7 @@ class WorkspaceDialogUpdateActivity : BaseActivity() {
     }
 
     private fun buildMessage(): Spanned {
-        val message = getString(R.string.use_internal_demo_or_select_template_folder)
+        val message = getString(R.string.welcome_screen_select_template_folder)
         return if (Build.VERSION.SDK_INT >= 24) {
             Html.fromHtml(message, 0)
         } else {
