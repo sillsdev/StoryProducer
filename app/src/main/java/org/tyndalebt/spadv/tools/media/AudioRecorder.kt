eae2a239a405656ed1750859fdb0ca825d2596ff
Lifeloop - TECH Team <info@lifeloop.live>
2022-08-15 14:02:26 -0500

    Limit archive audio to 3
    limit to last 3 audio files.  Also removes any audio files that are referenced in this project for whatever reason (crash, etc.)

--- app/src/main/java/org/tyndalebt/spadv/tools/media/AudioRecorder.kt
+++ app/src/main/java/org/tyndalebt/spadv/tools/media/AudioRecorder.kt
@@ -7,16 +7,19 @@ import android.content.pm.PackageManager
 import android.media.MediaMuxer
 import android.media.MediaRecorder
 import android.net.Uri
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import android.os.Environment
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.documentfile.provider.DocumentFile
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import org.tyndalebt.spadv.R
+import org.tyndalebt.spadv.model.PROJECT_DIR
+import org.tyndalebt.spadv.model.PhaseType
+import org.tyndalebt.spadv.model.Story
 import org.tyndalebt.spadv.model.Workspace
-import org.tyndalebt.spadv.tools.file.copyToWorkspacePath
-import org.tyndalebt.spadv.tools.file.getStoryFileDescriptor
-import org.tyndalebt.spadv.tools.file.getStoryUri
+import org.tyndalebt.spadv.tools.file.*
 import org.tyndalebt.spadv.tools.media.story.AutoStoryMaker
 import org.tyndalebt.spadv.tools.media.story.StoryMaker
 import org.tyndalebt.spadv.tools.media.story.StoryPage
@@ -30,6 +33,7 @@ internal val AUDIO_ENCODER = MediaRecorder.AudioEncoder.AAC
 internal val SAMPLE_RATE = 44100
 internal val BIT_DEPTH = 16
 internal val AUDIO_CHANNELS = 1
+internal val MAX_AUDIO_ARCHIVE_COUNT = 3
 //Set bit rate to exact spec of Android doc or to SAMPLE_RATE * BIT_DEPTH.
 internal val BIT_RATE = SAMPLE_RATE * BIT_DEPTH
 
@@ -97,7 +101,7 @@ class AudioRecorderMP4(activity: Activity) : AudioRecorder(activity) {
 
     private var mRecorder = MediaRecorder()
 
-    private fun initRecorder(){
+    private fun initRecorder() {
         mRecorder.release()
         mRecorder = MediaRecorder()
         mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
@@ -108,31 +112,30 @@ class AudioRecorderMP4(activity: Activity) : AudioRecorder(activity) {
         mRecorder.setAudioChannels(AUDIO_CHANNELS)
     }
 
-    override fun startNewRecording(relPath: String){
+    override fun startNewRecording(relPath: String) {
         initRecorder()
-        mRecorder.setOutputFile(getStoryFileDescriptor(activity, relPath,"","w"))
+        mRecorder.setOutputFile(getStoryFileDescriptor(activity, relPath, "", "w"))
         isRecording = true
-        try{
+        try {
             mRecorder.prepare()
             mRecorder.start()
-        }
-        catch (e: IllegalStateException) {
+        } catch (e: IllegalStateException) {
             Toast.makeText(activity, "IllegalStateException!", Toast.LENGTH_SHORT).show()
             FirebaseCrashlytics.getInstance().recordException(e)
-        }
-        catch (e: IOException) {
+        } catch (e: IOException) {
             Toast.makeText(activity, "IOException!", Toast.LENGTH_SHORT).show()
             FirebaseCrashlytics.getInstance().recordException(e)
         }
     }
 
     override fun stop() {
-        if(!isRecording) return
+        if (!isRecording) return
         try {
             mRecorder.stop()
             mRecorder.reset()
             mRecorder.release()
             isRecording = false
+            cleanupOlderFiles()
         } catch (stopException: RuntimeException) {
             Toast.makeText(activity, R.string.recording_toolbar_error_recording, Toast.LENGTH_SHORT).show()
             FirebaseCrashlytics.getInstance().recordException(stopException)
@@ -140,5 +143,103 @@ class AudioRecorderMP4(activity: Activity) : AudioRecorder(activity) {
             Log.e(AUDIO_RECORDER, "Voice recorder interrupted!", e)
         }
     }
-}
 
+    private fun isCurrentPhase(fileName: String?): Boolean {
+        var prefix: String = when (Workspace.activePhase.phaseType) {
+            PhaseType.TRANSLATE_REVISE -> {
+                "${Workspace.activePhase.getFileSafeName()}${Workspace.activeSlideNum}_"
+            }
+            PhaseType.VOICE_STUDIO -> {
+                "${Workspace.activePhase.getFileSafeName()}${Workspace.activeSlideNum}_"
+            }
+            PhaseType.BACK_T -> {
+                "${Workspace.activePhase.getFileSafeName()}${Workspace.activeSlideNum}_"
+            }
+            else -> {
+                return false // nothing
+            }
+        }
+        return fileName?.startsWith(prefix, false) == true
+    }
+
+    private fun getAudioFiles(phase: PhaseType): MutableList <String> {
+        when (Workspace.activePhase.phaseType) {
+            PhaseType.TRANSLATE_REVISE -> {
+                return Workspace.activeSlide!!.translateReviseAudioFiles
+            }
+            PhaseType.VOICE_STUDIO -> {
+                return Workspace.activeSlide!!.voiceStudioAudioFiles
+            }
+            PhaseType.BACK_T -> {
+                return Workspace.activeSlide!!.backTranslationAudioFiles
+            }
+            else -> {
+                var mList: MutableList <String> = arrayListOf()
+                return mList // nothing
+            }
+        }
+    }
+
+    private fun cleanupOlderFiles() {
+        var pos: Int? = null
+        var mCombName: String
+        var aSize: Int
+        do
+        {
+            aSize = getAudioFiles(Workspace.activePhase.phaseType).size
+            if (aSize <= MAX_AUDIO_ARCHIVE_COUNT)
+                break
+            pos = 0
+            val recordings = Workspace.activePhase.getCombNames()
+            mCombName = recordings!![pos]
+            val fileLocation = Story.getFilename(mCombName)
+            val filename = mCombName
+
+            recordings.removeAt(pos)
+            if (getChosenCombName() == filename) {
+                // current chosen WL has been deleted, shift the file index
+                if (recordings.size == 0) {
+                    setChosenFileIndex(-1)
+                }else {
+                    setChosenFileIndex(0)
+                }
+            }
+            // delete the recording file
+            deleteStoryFile(activity, fileLocation)
+        } while (aSize > MAX_AUDIO_ARCHIVE_COUNT)
+        if (aSize != 0)
+            cleanupUnreferencedFiles()
+    }
+
+    private fun cleanupUnreferencedFiles()
+    {
+        var docFile: DocumentFile? = null
+        var aList = getAudioFiles(Workspace.activePhase.phaseType)
+        if (aList.size < 1)
+            return
+        var found: Boolean
+        var title: String = Workspace.activeStory.title
+        docFile = Workspace.workdocfile.findFile(title)
+        docFile = docFile!!.findFile("project")
+        val audioList1: List<DocumentFile> = docFile!!.listFiles().filter { isCurrentPhase(it.name) }
+        if (audioList1.size <= MAX_AUDIO_ARCHIVE_COUNT)
+            return
+        var idx: Int = 0
+        do {
+            val name: String = audioList1[idx].name!!
+            var idx1: Int = 0
+            found = false
+            do {
+                if (aList[idx1].contains(name))
+                    found = true
+                idx1 += 1
+            } while (idx1 < aList.size)
+            if (!found) {
+
+                val fileName = docFile.findFile(name)
+                fileName!!.delete()
+            }
+            idx += 1
+        } while (idx < audioList1.size)
+    }
+}
