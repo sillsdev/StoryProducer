5f5203799fb14833141530e3c9918b0c018f204e
Peter C <ithinkapps@users.noreply.github.com>
2022-09-23 16:29:52 +0100

    Now using bl_samples.xml for Bloom Library sample catalog loading

--- app/src/main/java/org/sil/storyproducer/model/BLBookList.kt
+++ app/src/main/java/org/sil/storyproducer/model/BLBookList.kt
@@ -33,7 +33,12 @@ open class BLBookList(var dateUpdated: Date) {
 
         const val WIFI = "Wi-Fi"
         const val ANY = "Any"
-        const val BLOOM_LIB_URL = "https://sil-storyproducer-resources.s3.amazonaws.com/bl1/bl_catalog.xml"
+
+        private const val resourceBucket = "sil-storyproducer-resources"
+        private const val resourceDomain = "s3.amazonaws.com"
+        private const val resourceKey = "bl1/bl_samples.xml"
+        val resourceAddr = "https://${resourceBucket}.${resourceDomain}/${resourceKey}"
+
         // Whether there is a Wi-Fi connection.
         private var wifiConnected = true//false
         // Whether there is a mobile connection.
@@ -47,25 +52,6 @@ open class BLBookList(var dateUpdated: Date) {
 
     // We don't use namespaces
     private val ns: String? = null
-
-    init {
-//        booklist.add(BLBook("001 The Widow’s Offering", "en",
-//            "https://api.bloomlibrary.org/v1/fs/harvest/A7cc3l3E5u/thumbnails/thumbnail-256.png?version=2022-08-17T17:36:38.898Z&amp;ref=sil-spapp",
-//            "https://api.bloomlibrary.org/v1/fs/harvest/A7cc3l3E5u/001+The+Widow%e2%80%99s+Offering.bloomSource?ref=sil-spapp"))
-//        booklist.add(BLBook("002 Lost Coin", "en",
-//            "https://api.bloomlibrary.org/v1/fs/harvest/axp6zFqdGH/thumbnails/thumbnail-256.png?version=2022-08-17T17:53:50.678Z&amp;ref=sil-spapp",
-//            "https://api.bloomlibrary.org/v1/fs/harvest/axp6zFqdGH/002+Lost+Coin.bloomSource?ref=sil-spapp"))
-//        booklist.add(BLBook("006 Snakes Secret", "en",
-//            "https://api.bloomlibrary.org/v1/fs/harvest/pdOADFmREf/thumbnails/thumbnail-256.png?version=2022-08-17T18:03:02.753Z&amp;ref=sil-spapp",
-//            "https://api.bloomlibrary.org/v1/fs/harvest/pdOADFmREf/006+Snakes+Secret.bloomSource?ref=sil-spapp"))
-//        booklist.add(BLBook("007 Leopards Kill", "en",
-//            "https://api.bloomlibrary.org/v1/fs/harvest/lE7Gj1se23/thumbnails/thumbnail-256.png?version=2022-08-17T17:59:58.649Z&amp;ref=sil-spapp",
-//            "https://api.bloomlibrary.org/v1/fs/harvest/lE7Gj1se23/007+Leopards+Kill.bloomSource?ref=sil-spapp"))
-//        booklist.add(BLBook("103 Babel Languages", "en",
-//            "https://api.bloomlibrary.org/v1/fs/harvest/MDyjSjSmPe/thumbnails/thumbnail-256.png?version=2022-08-17T17:45:22.243Z&amp;ref=sil-spapp",
-//            "https://api.bloomlibrary.org/v1/fs/harvest/MDyjSjSmPe/103+Babel+Languages.bloomSource?ref=sil-spapp"))
-    }
-
 
     //
     // xml parsing code methods below are adapted from: https://developer.android.com/training/basics/network-ops/xml
@@ -272,9 +258,9 @@ open class BLBookList(var dateUpdated: Date) {
     fun loadPage() {
 
         if (sPref.equals(ANY) && (wifiConnected || mobileConnected)) {
-            DownloadXmlTask().execute(BLOOM_LIB_URL)
+            DownloadXmlTask().execute(resourceAddr)
         } else if (sPref.equals(WIFI) && wifiConnected) {
-            DownloadXmlTask().execute(BLOOM_LIB_URL)
+            DownloadXmlTask().execute(resourceAddr)
         } else {
             // show error
         }
