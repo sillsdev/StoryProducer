63b703835c6303a4c3034b604417869b966c12fe
Bryan Wussow <bryan.wussow@sil.org>
2022-09-26 12:03:21 -0400

    Getting Started UX for v3.2 (#664)
    * Getting Started UX for v3.2

--- app/src/main/java/org/sil/storyproducer/tools/DrawerItemClickListener.kt
+++ app/src/main/java/org/sil/storyproducer/tools/DrawerItemClickListener.kt
@@ -43,22 +43,25 @@ class DrawerItemClickListener(private val activity: BaseActivity) : AdapterView.
                 activity.showMain()
             }
             2 -> {
+                activity.showBLDownloadDialog()
+            }
+            3 -> {
                 // DKH - 01/23/2022 Issue #571: Add a menu item for accessing templates from Google Drive
                 // A new menu item was added that opens a URL for the user to download templates.
                 // If we get here, the user wants to browse for more templates, so,
                 // open the URL in a new activity
                 Workspace.startDownLoadMoreTemplatesActivity(activity)
             }
-            3 -> {
+            4 -> {
                 activity.showWordLinksList()
             }
-            4 -> {
+            5 -> {
                 activity.showSelectTemplatesFolderDialog()
             }
-            5 -> {
+            6 -> {
                 Workspace.addDemoToWorkspace(activity)
             }
-            6 -> {
+            7 -> {
                 activity.showAboutDialog()
             }
         }
