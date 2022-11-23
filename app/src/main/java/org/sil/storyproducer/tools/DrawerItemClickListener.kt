7cd069ed48630eef8c95cf31ce251fbf8a5eb79d
Peter C <ithinkapps@users.noreply.github.com>
2022-11-23 13:34:02 +0000

    Implemented: Remove registration prompt on startup

--- app/src/main/java/org/sil/storyproducer/tools/DrawerItemClickListener.kt
+++ app/src/main/java/org/sil/storyproducer/tools/DrawerItemClickListener.kt
@@ -27,29 +27,13 @@ class DrawerItemClickListener(private val activity: BaseActivity) : AdapterView.
     private fun selectItem(position: Int) {
         when (position) {
             0 -> {
-                var intent = Intent(activity.applicationContext, MainActivity::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                activity.startActivity(intent)
-                activity.finish()
+                activity.finish()   // finish this activity to reveal main activity
             }
             1 -> {
-                // 06/14/2021 - DKH, Issue 407, Pull Request 561 - Merge into Latest sillsdev
-                // The new Filter layout for the Story Templates broke the capability to launch
-                // the showRegistration () from the calling activity.  SP uses the paradigm of
-                // always finishing an activity and then moving onto the next activity.  In this case
-                // it would be to finish the current activity and move on to the
-                // Registration Activity.  However, when a submit is
-                // done from registration, another application such as WhatsApp or Messenger takes
-                // over and when they finished, they exit without starting another SP activity.
-                // This causes SP to hang in the Splash Activity with no where to go.  This is
-                // similar to the issue fixed in
-                // Issue #573: SP will hang/crash when submitting registration
-                // In that fix, the Main activity does not finish before starting the registration
-                // activity.  So, employ the same fix, ie, tell the main activity to display the
-                // registration when the main activity is started and then start the main activity
-                // See comments in MainActivity for more details
-                Workspace.showRegistration = true
-                activity.showMain()
+                // Showing registration on top of the MainActivity in the task stack
+                // This should allow registering without finishing the main activity
+                // and without hanging in the splash screen as previously experienced
+                activity.showRegistration(true)
             }
             2 -> {
                 activity.showBLDownloadDialog()
@@ -69,7 +53,7 @@ class DrawerItemClickListener(private val activity: BaseActivity) : AdapterView.
             }
             5 -> {
                 Workspace.addDemoToWorkspace(activity)
-                MainActivity.mainActivity.controller.updateStories()  // refresh list of stories
+                activity.controller.updateStories()  // refresh list of stories
             }
             6 -> {
                 activity.showAboutDialog()
