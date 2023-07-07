69d7aee7ac599108ed14163ec39fa6d70e90f913
Peter C <ithinkapps@users.noreply.github.com>
2023-07-07 12:17:44 +0100

    Initial unit tests added for importing an SP-Authored book/Template

---
+++ app/src/test/java/org/sil/storyproducer/model/ParseBloomTest.kt
@@ -0,0 +1,59 @@
+package org.sil.storyproducer.model
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+// import org.mockito.Mockito
+import org.robolectric.RobolectricTestRunner
+
+import androidx.test.core.app.ApplicationProvider
+
+import java.io.File
+
+// See below for some background to Roboelectric
+// https://www.vogella.com/tutorials/Robolectric/article.html
+
+@RunWith(RobolectricTestRunner::class)
+class ParseBloomTest {
+
+    var result: Story? = null   // saved 'one-time' parsed story for validation with each test
+
+    init {
+        // WARNING: parsing Bloom HTML Templates is not just read-only - in for some stories extra concatenated audio files could be produced
+        result = parse_SPAT_Story() // parse the SP authored template once, on class initialization
+    }
+
+    @Test
+    fun parsed_SPAT_Should_ReturnAStory() {
+        Assert.assertNotNull(result)
+        Assert.assertEquals(Story::class.java, result!!.javaClass)
+    }
+
+//    @Test
+    fun parsed_SPAT_Should_get_isSPAuthored_True() {
+        // This currently cannot be tested as isSPAuthored has not been added as a member of the Story class
+        // Assert.assertTrue(result!!.???)
+    }
+
+    @Test
+    fun parsed_SPAT_Should_get_StoryTitleFromBookFrontCover() {
+        Assert.assertEquals("000 Unlocked demo", result!!.title)
+    }
+
+    private fun parse_SPAT_Story(): Story? {
+        setupWorkspace()
+        val storyPath = Workspace.workdocfile.findFile("000 Unlocked demo")
+        return parseBloomHTML(ApplicationProvider.getApplicationContext(), storyPath!!)
+    }
+
+    private fun setupWorkspace() {
+        println(System.getProperty("user.dir"))
+        // NB: Using the unlocked demo story for now but in the future we should probably have dedicated test templates under git
+        var df = androidx.documentfile.provider.DocumentFile.fromFile(File("app/src/main/assets"))
+        if(!df.isDirectory){
+            df = androidx.documentfile.provider.DocumentFile.fromFile(File("src/main/assets"))
+        }
+        Workspace.workdocfile = df
+    }
+
+}
