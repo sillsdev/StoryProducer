9c97306d5964e7be26f2d35f9e80839b2ef87c53
Peter C <ithinkapps@users.noreply.github.com>
2024-09-30 12:55:31 +0100

    Task: 167 - Hide BTW during ‘now creating your video’ status screen (amended some comments)

--- app/src/main/java/org/sil/storyproducer/controller/export/FinalizeActivity.kt
+++ app/src/main/java/org/sil/storyproducer/controller/export/FinalizeActivity.kt
@@ -371,10 +371,10 @@ class FinalizeActivity : PhaseBaseActivity() {
         mLayoutConfiguration.visibility = visibilityPreExport
         mLayoutCancel.visibility = visibilityWhileExport
         if (mLayoutCancel.visibility == View.VISIBLE) {
-            // cancel layout is visible so dismiss any help popups
+            // 'cancel layout' is visible so dismiss any help popups
             mPopupHelpUtils?.dismissPopup()
         } else {
-            // cancel layout now visible so resume any previously showing help popups
+            // 'cancel layout' now invisible again so resume any previously showing help popups
             mPopupHelpUtils?.showNextPopupHelp()
         }
         mButtonStart.visibility = visibilityPreExport
